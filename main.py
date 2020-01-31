6a204a70ba1d12123ea9a8a4e5a36a00bb41a73e
Jesus Fernando Gutierrez García <jesusf.gutierrez@softtekclients.com>
2020-01-31 14:58:41 -0800

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Name                  : main.py
+# Creators              : Fernando Gutierrez        
+# github token: d238b454a4e0c46ced04c576a7de34fd4aa73039
+#from urllib.parse import quote, urlencode
+import os
+
+
+print("Building project")
+print("...")
+f= open("artifact.txt","w")
+f.write("My file")
+f.close() 
+
